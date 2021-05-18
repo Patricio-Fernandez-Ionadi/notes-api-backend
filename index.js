18142b59e8381c72de3b4a003c84d787158a6bca
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2021-05-18 19:47:34 -0300

    modularizacion manejo de errores

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 require("./mongo")
 const Note = require("./models/Note")
 const express = require("express")
@@ -6,8 +7,6 @@ const cors = require("cors")
 
 app.use(cors())
 app.use(express.json())
-
-let notes = []
 
 app.get("/", (req, res) => {
 	res.send("<h1>Hello home</h1>")
@@ -19,21 +18,42 @@ app.get("/api/notes", (req, res) => {
 	})
 })
 
-app.get("/api/notes/:id", (req, res) => {
-	const id = Number(req.params.id)
-	const note = notes.find((note) => note.id === id)
-
-	if (note) {
-		res.json(note)
-	} else {
-		res.status(404).end()
-	}
+app.get("/api/notes/:id", (req, res, next) => {
+	const { id } = req.params
+	Note.findById(id)
+		.then((note) => {
+			if (note) {
+				return res.json(note)
+			} else {
+				res.status(404).end()
+			}
+		})
+		.catch((err) => {
+			next(err)
+		})
 })
 
-app.delete("/api/notes/:id", (req, res) => {
-	const id = Number(req.params.id)
-	notes = notes.filter((note) => note.id !== id)
-	res.status(204).end()
+app.put("/api/notes/:id", (req, res, next) => {
+	const { id } = req.params
+	const note = req.body
+
+	const newNoteInfo = {
+		content: note.content,
+		important: note.important,
+	}
+	// cuando se usa el metodo findByIdAndUpdate() lo que nos devuelve es lo que ha encontrado por id y no el nuevo contenido actualizado
+	// le pasamos un objeto como tercer parametro con la key 'new' y el value true, y asi nos devolverá el objeto actualizado
+	Note.findByIdAndUpdate(id, newNoteInfo, { new: true }).then((response) => {
+		res.json(response)
+	})
+})
+
+app.delete("/api/notes/:id", (req, res, next) => {
+	const { id } = req.params
+
+	Note.findByIdAndDelete(id)
+		.then(() => res.status(204).end())
+		.catch((err) => next(err))
 })
 
 app.post("/api/notes", (req, res) => {
@@ -45,27 +65,39 @@ app.post("/api/notes", (req, res) => {
 		})
 	}
 
-	const ids = notes.map((each) => each.id)
-	const maxId = Math.max(...ids)
-
-	const newNote = {
-		id: maxId + 1,
+	const newNote = new Note({
 		content: note.content,
 		important: typeof note.important !== "undefined" ? note.important : false,
 		date: new Date().toISOString(),
-	}
+	})
 
-	notes = [...notes, newNote]
+	newNote.save().then((savedNote) => {
+		res.json(savedNote)
+	})
+
 	res.status(201).json(newNote)
 })
 
-app.use((req, res) => {
-	res.status(404).json({
-		error: "Not found",
-	})
-})
+// moveremos esto a un archivo externo por ejemplo notFound.js en una carpeta middlewares
+// app.use((req, res, next) => {
+// 	res.status(404).end()
+// })
+const notFound = require("./middlewares/notFound") // esto deberia importarse arriba
+app.use(notFound)
 
-const PORT = process.env.PORT || 3001
+// entrará a este middleware cuando llegue un error como parametro
+// app.use((err, req, res, next) => {
+// 	console.error(err)
+// 	if (err.name === "CastError") {
+// 		res.status(400).send({ error: "id used is malformed" })
+// 	} else {
+// 		res.status(500).end()
+// 	}
+// })
+const handleErrors = require("./middlewares/handleErrors")
+app.use(handleErrors)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose")
-const password = require("./password")
 
-const connectionString = `mongodb://poker:${password}@cluster0-shard-00-00.gvn3i.mongodb.net:27017,cluster0-shard-00-01.gvn3i.mongodb.net:27017,cluster0-shard-00-02.gvn3i.mongodb.net:27017/cluster0?ssl=true&replicaSet=atlas-2qn6om-shard-0&authSource=admin&retryWrites=true&w=majority`
+const connectionString = process.env.MONGO_DB_URI
 
 // conexion a mongo
 mongoose
